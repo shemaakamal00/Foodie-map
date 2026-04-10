6291827a62e38b68fa5342d3357403eeb260bf19
Kareem D <drammeh@msn.com>
2026-04-11 00:44:32 +0700

    Fixed e2e favorites test so buttons test correctly in mobile-view

--- tests/e2e/favorites.test.ts
+++ tests/e2e/favorites.test.ts
@@ -1,16 +1,50 @@
 import { test, expect } from "@playwright/test";
 
-test("user can open restaurant and go back through box right and left", async ({ page }) => {
-  await page.goto("http://localhost:5173/favoritsida.html");
-  await expect(page.locator("body")).toBeVisible();
-  await page.waitForTimeout(2000);
-  
-   const testCards = page.locator(".card");
-   if (await testCards.count() === 0) {
-    test.skip(); 
-  }
-  await testCards.first().click();
-  await expect(page.locator(".box-right")).toBeVisible();
-  await page.locator("#back-button").click();
-  await expect(page.locator(".box-left")).toBeVisible();
+// Kör test i mobilvy så knappen syns i playwright
+test.use({ viewport: { width: 375, height: 667 } });
+
+test("user can open favorite and go back", async ({ page }) => {
+
+ await page.route("**/rest/v1/restaurants*", async (route) => {
+    await route.fulfill({
+      status: 200,
+      contentType: "application/json",
+      body: JSON.stringify([
+        {
+          id: 1,
+          name: "Test Restaurant",
+          address: "Testgatan 1",
+          description: "Test",
+          image_url: "https://via.placeholder.com/150",
+          latitude: 59,
+          longitude: 18,
+          diets: ["Halal"]
+        }
+      ])
+    });
+  });
+
+  await page.route("**/rest/v1/favorites*", async (route) => {
+    await route.fulfill({
+      status: 200,
+      contentType: "application/json",
+      body: JSON.stringify([
+        {
+          restaurant_id: 1,
+          device_id: "test"
+        }
+      ])
+    });
+  });
+
+  //Sätt namn "test" i device_id så data kan hittas
+  await page.addInitScript(() => {
+    localStorage.setItem("device_id", "test");
+  });
+await page.goto("/favoritsida.html");
+await page.waitForSelector(".card");
+await page.locator(".card").click();
+await page.waitForSelector(".container.show-detail");
+await page.locator("#back-button").click();
+await expect(page.locator(".container")).not.toHaveClass(/show-detail/);
 });
